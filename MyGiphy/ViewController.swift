26046b6647aadbb7e2f78cebc5bf5c3bd305e65e
Benjamin Dodgson <benji@tomorrow.me>
2019-08-16 13:50:20 -0700

    Initial Commit

---
+++ MyGiphy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGiphy
+//
+//  Created by Benji Dodgson on 8/16/19.
+//  Copyright © 2019 Tomorrow Ideas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
