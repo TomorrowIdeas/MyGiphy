b334837b4b11862d755294cab3e3209657d3f328
normdoow <noahbragg@cedarville.edu>
2019-08-19 15:11:56 -0400

    added extension to help with setting constraints

---
+++ MyGiphy/view/extensions/UIViewExtension.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewExtension.swift
+//  MyGiphy
+//
+//  Created by Noah Bragg on 8/19/19.
+//  Copyright © 2019 Tomorrow Ideas. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
