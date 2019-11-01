c28c64346609878edd6bb52c0819cd987accc9ba
Eugene Berezin <berezin.nlp@gmail.com>
2019-10-31 18:33:10 -0700

    Installing pods, first try playing around with giphy

--- MyGiphy/ViewController.swift
+++ MyGiphy/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import GiphyUISDK
+import GiphyCoreSDK
 
 class ViewController: UIViewController {
+    let giphy = GiphyViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-        //first commit
+        view.backgroundColor = .white
+         
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true )
+        present(giphy, animated: true, completion: nil)
+        giphyConfig()
+    }
+    
+    func giphyConfig() {
+        GiphyUISDK.configure(apiKey: "totF40WvhWTaFQzaNNl6n0okld9PPFHT")
+        
+        giphy.theme = .dark
+        giphy.layout = .waterfall
+        giphy.mediaTypeConfig = [.gifs, .stickers, .text, .emoji]
+        giphy.showConfirmationScreen = true
+        giphy.rating = .ratedPG13
+        giphy.renditionType = .fixedWidth
+        giphy.shouldLocalizeSearch = false
+        GiphyViewController.trayHeightMultiplier = 0.7
+        
+    
+        
     }
 }
 
